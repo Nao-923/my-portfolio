3c396775dfe051471a7f54878171107b161e066e
Nao-923 <110517626+Nao-923@users.noreply.github.com>
2025-02-14 01:19:22 +0900

    修正

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     domains: ["prod-files-secure.s3.us-west-2.amazonaws.com"],
   },
